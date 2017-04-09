9c0da1bd665d1e38d6591059aa4dd2f1634a1f44
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-04-10 00:16:42 +0100

    Added goal rotate

---
+++ Assets/GoalRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalRotate : MonoBehaviour {
+    public bool goalRotate;
+    public bool isClockwise;
+    private float m_speed = 25f;
+	// Use this for initialization
+	void Start ()
+    {
+		if(!isClockwise)
+        {
+            m_speed = m_speed * -1f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(goalRotate)
+        {
+            transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + Time.deltaTime * m_speed);
+        }
+	}
+}
